2a9a4b109d4258794069f2ae1f2d00d7332c37f7
Samyar <samyar1380@gmail.com>
2022-12-18 00:26:07 +0330

    Database
    - Built a database using sqlite
    - Added a Todo model to record basic data for every task

--- Main/app.py
+++ Main/app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+with app.app_context():
+    db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{id} - {self.body} created at {self.date_created}"
 
 @app.route('/')
 def index():
